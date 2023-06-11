56340c93ab1a69e38faf49a1a5ef825b6d06e02b
Jashim Uddin <jashim.official75@gmail.com>
2023-06-11 19:55:15 +0600

    3 more card added

--- script.js
+++ script.js
@@ -48,26 +48,13 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+
 });
+
+var viewmorebtn = document.getElementById("view_moreBtn");
+var viewmorecard = document.getElementById("view-more");
+
+viewmorebtn.addEventListener("click",()=>{
+    viewmorecard.classList.toggle("active");
+    viewmorebtn.style.display = "none";
+})
